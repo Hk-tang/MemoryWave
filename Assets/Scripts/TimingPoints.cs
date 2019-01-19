9dd20df7d56e638525a7bae67fa585c1e25bc9f7
hiufungk <hiufungk@ualberta.ca>
2019-01-19 14:04:36 -0700

    added timing points class

---
+++ Assets/Scripts/TimingPoints.cs
@@ -0,0 +1,18 @@
+﻿
+public class TimingPoints
+{
+    private int offset;
+    private double msPerBeat;
+    private int beatsPerMeasure;
+    private int volume;
+    private int playmode;
+
+    public TimingPoints(int offset, double msPerBeat, int beatsPerMeasure, int volume, int playmode)
+    {
+        this.offset = offset;
+        this.msPerBeat = msPerBeat;
+        this.beatsPerMeasure = beatsPerMeasure;
+        this.volume = volume;
+        this.playmode = playmode;
+    }
+}
