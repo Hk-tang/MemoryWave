6c1cdf87708839799b271722b8c935740c9b0b71
Henry Tang <hktang@ualberta.ca>
2019-01-19 18:34:47 -0700

    Added scrollable song selection

--- Assets/Scripts/SongSelectParser.cs
+++ Assets/Scripts/SongSelectParser.cs
@@ -1,82 +1,76 @@
-﻿using System;
-using System.IO;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
 
-public class SongSelectParser
+public class SongSelectParser : MonoBehaviour
 {
-	public static void Main()
+    public ScrollRect scrollView;
+    public GameObject scrollContent;
+    public GameObject scrollItemPrefab;
+
+    static HashSet<string> metaInfo = new HashSet<string>
+        {
+            "AudioFilename",
+            "PreviewTime",
+            "Title",
+            "TitleUnicode",
+            "Artist",
+            "ArtistUnicode",
+            "Creator",
+            "Version",
+            "Source",
+            "Tags",
+            "LevelID",
+            "LevelSetID",
+            "HPDrainRate",
+            "OverallDifficulty",
+            "ApproachRate"
+        };
+
+    void GenerateSongButton(Dictionary<string, string> song)
     {
-        List<string[]> songs = new List<string[]>();
-        foreach (string file in Directory.EnumerateFiles("..\\Songs", "*.memw"))
+        
+        GameObject songButton = Instantiate(scrollItemPrefab);
+        songButton.transform.SetParent(scrollContent.transform, false);
+        songButton.transform.Find("SongTitle").gameObject.GetComponent<Text>().text = song["Title"];
+
+    }
+
+    List<Dictionary<string, string>> CreateSongList()
+    {
+        List<Dictionary<string, string>> songs = new List<Dictionary<string, string>>();
+        
+        foreach (string file in Directory.EnumerateFiles(".\\Songs", "*.memw"))
         {
             string[] lines = File.ReadAllLines(file);
-            string[] metaInfo = new string[15];
+            var songInfo = new Dictionary<string, string>();
+            
             foreach (string line in lines)
             {
                 var parts = line.Split(':');
-                if(parts[0] == "AudioFilename")
+                if (metaInfo.Contains(parts[0]))
                 {
-                    metaInfo[0] = parts[1];
-                }
-                else if (parts[0] == "PreviewTime") 
-                {
-                    metaInfo[1] = parts[1];
-                }
-                else if (parts[0] == "Title")
-                {
-                    metaInfo[2] = parts[1];
-                }
-                else if (parts[0] == "TitleUnicode")
-                {
-                    metaInfo[3] = parts[1];
-                }
-                else if (parts[0] == "Artist")
-                {
-                    metaInfo[4] = parts[1];
-                }
-                else if (parts[0] == "ArtistUnicode")
-                {
-                    metaInfo[5] = parts[1];
-                }
-                else if (parts[0] == "Creator")
-                {
-                    metaInfo[6] = parts[1];
-                }
-                else if (parts[0] == "Version")
-                {
-                    metaInfo[7] = parts[1];
-                }
-                else if (parts[0] == "Source")
-                {
-                    metaInfo[8] = parts[1];
-                }
-                else if (parts[0] == "Tags")
-                {
-                    metaInfo[9] = parts[1];
-                }
-                else if (parts[0] == "LevelID")
-                {
-                    metaInfo[10] = parts[1];
-                }
-                else if (parts[0] == "LevelSetID")
-                {
-                    metaInfo[11] = parts[1];
-                }
-                else if (parts[0] == "HPDrainRate")
-                {
-                    metaInfo[12] = parts[1];
-                }
-                else if (parts[0] == "OverallDifficulty")
-                {
-                    metaInfo[13] = parts[1];
-                }
-                else if (parts[0] == "ApproachRate")
-                {
-                    metaInfo[14] = parts[1];
+                    songInfo[parts[0]] = parts[1];
                 }
             }
 
-            songs.Add(metaInfo);
+            songs.Add(songInfo);
         }
+
+        return songs;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Dictionary<string, string>> songs = CreateSongList();
+        foreach (Dictionary<string, string> song in songs)
+        {
+            GenerateSongButton(song);
+        }
+
+        scrollView.verticalNormalizedPosition = 1;
     }
 }
