3bde00df82180ac8851890e82a9b57391a0f0184
Hk-tang <hktang@ualberta.ca>
2019-01-20 05:30:52 -0700

    Removed Debug statements

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -26,7 +26,6 @@ public class NoteObject : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.tag);
         if (collision.tag == "cats")
         {
             hit = true;
