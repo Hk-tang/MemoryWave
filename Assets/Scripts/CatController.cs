c12845c7f217c6226e2d0d96ab81d65d4d73d925
shchau <shchau1@ualberta.ca>
2019-01-19 23:18:56 -0700

    combined my stuff with Kevin's fully. Looks pretty fkd up though

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -20,8 +20,6 @@ public class CatController : MonoBehaviour
 	AudioSource[] bongoSounds;
 	
 	
-	SimpleSpectrum test;
-	
     // Start is called before the first frame update
     void Start()
     {
